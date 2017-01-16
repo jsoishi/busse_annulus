ad950f1fd4754af542acb959f9fb07a695502918
J.S. Oishi <jsoishi@gmail.com>
2017-01-16 17:52:54 -0500

    added ny option and CFL option

--- python/busse_annulus.py
+++ python/busse_annulus.py
@@ -3,7 +3,7 @@
 ref: Brummell & Hart (1993) fig 5a
 
 Usage:
-    busse_annulus.py [--Ra=<Ra> --beta=<beta> --C=<C> --Pr=<Pr> --restart=<restart_file> --nx=<nx> --filter=<filter> --seed=<seed>] 
+    busse_annulus.py [--Ra=<Ra> --beta=<beta> --C=<C> --Pr=<Pr> --restart=<restart_file> --nx=<nx> --ny=<ny> --filter=<filter> --seed=<seed> --use-CFL] 
 
 Options:
     --Ra=<Ra>      Rayleigh number [default: 39000]
@@ -12,8 +12,10 @@ Options:
     --C=<C>        C parameter [default: 0]
     --restart=<restart_file>   Restart from checkpoint
     --nx=<nx>                  x (Fourier) resolution [default: 128]
+    --ny=<ny>                  y (Sin/Cos) resolution [default: 128]
     --filter=<filter>          fraction of modes to keep in ICs [default: 0.5]
     --seed=<seed>  random seed for ICs [default: None]
+    --use-CFL                  use CFL condition
 """
 import os
 import sys
@@ -40,7 +42,7 @@ args = docopt(__doc__)
 
 # Parameters
 nx = int(args['--nx']) # resolution
-ny = nx
+ny = int(args['--ny']) # resolution
 Lx, Ly = (2*np.pi, 1)
 
 Ra = float(args['--Ra'])
@@ -51,6 +53,7 @@ C = float(args['--C'])
 
 restart = args['--restart']
 seed = args['--seed']
+CFL = args['--use-CFL']
 
 if seed == 'None':
     seed = None
@@ -59,7 +62,11 @@ else:
 
 # save data in directory named after script
 data_dir = "scratch/" + sys.argv[0].split('.py')[0]
-data_dir += "_ra{0:5.02e}_beta{1:5.02e}_C{2:5.02e}_Pr{3:5.02e}_filter{4:5.02e}_nx{5:d}/".format(Ra, beta, C, Pr, filter_frac,nx)
+data_dir += "_ra{0:5.02e}_beta{1:5.02e}_C{2:5.02e}_Pr{3:5.02e}_filter{4:5.02e}_nx{5:d}_ny{6:d}".format(Ra, beta, C, Pr, filter_frac,nx,ny)
+
+if CFL:
+    data_dir += "_CFL"
+
 
 if MPI.COMM_WORLD.rank == 0:
     if not os.path.exists('{:s}/'.format(data_dir)):
@@ -71,7 +78,7 @@ start_init_time = time.time()
 
 x_basis = de.Fourier('x', nx, interval=(0, Lx), dealias=3/2)
 y_basis = de.SinCos('y', ny, interval=(0, Ly), dealias=3/2)
-domain = de.Domain([x_basis, y_basis], grid_dtype=np.float64)
+domain = de.Domain([y_basis, x_basis], grid_dtype=np.float64)
 
 # 2D Boussinesq hydrodynamics
 problem = de.IVP(domain, variables=['psi','theta','zeta'], time='t')
@@ -86,7 +93,7 @@ problem.parameters['C'] = C
 problem.substitutions['J(A,B)'] = "dx(A) * dy(B) - dy(A) * dx(B)"
 
 problem.add_equation("dt(zeta) - beta*dx(psi) + Ra/Pr * dx(theta) + C * sbeta * zeta - dx(dx(zeta)) - dy(dy(zeta)) = -J(psi,zeta)", condition="ny != 0")
-problem.add_equation("dt(theta) + dx(psi) - (dx(dx(theta)) + dy(dy(theta)))/Pr = -J(psi,theta)", condition="ny != 0")
+problem.add_equation("dt(theta) + dx(psi)/Pr - (dx(dx(theta)) + dy(dy(theta)))/Pr = -J(psi,theta)", condition="ny != 0")
 problem.add_equation("dx(dx(psi)) + dy(dy(psi)) - zeta = 0", condition="ny != 0")
 problem.add_equation("zeta = 0", condition="ny ==0")
 problem.add_equation("theta = 0", condition="ny ==0")
@@ -126,7 +133,13 @@ else:
     pert =  1e-3 * rand.standard_normal(shape) #* (yt - y) * (y - yb)
     theta['g'] = pert
 
-    dt = 1e-4
+    if CFL:
+        CFL = flow_tools.CFL(solver, initial_dt=1e-4, cadence=5, safety=0.3,
+                             max_change=1.5, min_change=0.5)
+        CFL.add_velocities(('dy(psi)', '-dx(psi)'))
+        dt = CFL.compute_dt()
+    else:
+        dt = 1e-4
     snap_count  = 1
     snap_set    = 1
     integ_count = 1
@@ -138,9 +151,9 @@ if checkpointing:
     chk.set_checkpoint(solver,wall_dt=15.,write_num=chk_write, set_num=chk_set)
 
 # Integration parameters
-solver.stop_sim_time = 20.
+solver.stop_sim_time = 100.
 solver.stop_wall_time = 60.*60
-solver.stop_iteration = 2000 #np.inf
+solver.stop_iteration = np.inf
 
 # Analysis
 analysis_tasks = []
@@ -159,8 +172,8 @@ integ.add_task("integ(zeta, 'x')", name='<vorticity>_x', scales=1)
 analysis_tasks.append(integ)
 
 timeseries = solver.evaluator.add_file_handler(os.path.join(data_dir,'timeseries'), sim_dt=1e-3, write_num=ts_count, set_num=ts_set)
-timeseries.add_task("integ(integ(dx(psi)**2 + dy(psi)**2,'x'),'y')",name='Ekin')
-timeseries.add_task("integ(integ(dy(psi)**2,'x'),'y')",name='E_zonal')
+timeseries.add_task("0.5*integ(integ(dx(psi)**2 + dy(psi)**2,'x'),'y')",name='Ekin')
+timeseries.add_task("0.5*integ(integ(dy(psi)**2,'x'),'y')",name='E_zonal')
 timeseries.add_task("2*Ra/Pr * integ(integ(psi*dx(theta),'x'),'y') - 2*integ(integ((dx(dx(zeta)) + dy(dy(zeta)))**2,'x'),'y')", name="dEdt")
 analysis_tasks.append(timeseries)
 
@@ -177,6 +190,8 @@ try:
         if (solver.iteration-1) % 100 == 0:
             logger.info('Iteration: %i, Time: %e, dt: %e' %(solver.iteration, solver.sim_time, dt))
             logger.info('Max E_kin = %f' %flow.max('Ekin'))
+        if CFL:
+            dt = CFL.compute_dt()
 except:
     logger.error('Exception raised, triggering end of main loop.')
     raise
