6ece4f29b88c3350516168ccb4d1dff33fb1b0d6
J.S. Oishi <jsoishi@gmail.com>
2017-01-24 16:40:21 -0500

    take centered diff to get dE_Z/dt from numerical E_Z data; update plots accordingly.

--- python/plot_phase_energy_zeta.py
+++ python/plot_phase_energy_zeta.py
@@ -14,6 +14,10 @@ plt.style.use('ggplot')
 mpl.rcParams['font.size'] = 18
 datadir = sys.argv[-1]
 
+def central_diff(t, data):
+    diff = (data[2:] - data[0:-2])/(t[2:] - t[0:-2])
+    return diff[:,0]
+
 ts = h5py.File(datadir+"/timeseries/timeseries_s1.h5")
 snapshots = h5py.File(datadir+"/snapshots/snapshots_s1.h5")
 
@@ -24,17 +28,25 @@ vort_ax = fig.add_axes([0.08,0.1,0.8,0.4])
 cb_ax = fig.add_axes([0.88,0.1,0.01,0.4])
 
 
-phase_ax = fig.add_axes([0.08,0.6,0.35,0.35])
+phase_ax = fig.add_axes([0.1,0.6,0.35,0.35])
 ts_ax = fig.add_axes([0.55,0.6,0.35,0.35])
 
-A = 2*np.pi*1
-ts_ax.plot(ts['scales/sim_time'][:],ts['tasks/E_zonal'][:,0]/A)
-ts_ax.set_xlabel('time')
-ts_ax.set_ylabel('Zonal Kinetic Energy')
+ts_ax.plot(ts['scales/sim_time'][:],2*ts['tasks/E_zonal2'][:,0])
+ts_ax.set_xlim(9,10)
+ts_ax.set_ylim(650,950)
+ts_ax.set_xlabel('$t$')
+ts_ax.set_ylabel('$E_Z$')
 
-phase_ax.plot(ts['scales/sim_time'][:],ts['tasks/dEdt'][:,0])
-phase_ax.set_xlabel('time')
-phase_ax.set_ylabel('dE/dt')
+dedt = central_diff(ts['scales/sim_time'][:],2*ts['tasks/E_zonal2'][:,0])
+
+t_trim = ts['scales/sim_time'][1:-1]
+ez = ts['tasks/E_zonal2'][1:-1,0]
+t_select = (t_trim >9)
+phase_ax.scatter(2*ez[t_select],2*dedt[t_select]/1000)
+phase_ax.set_xlim(750,850)
+phase_ax.set_xlabel('$E_Z$')
+phase_ax.set_ylabel('$10^{-3}\ dE_Z/dt$')
+phase_ax.get_xaxis().get_major_formatter().set_useOffset(True)
 
 im = vort_ax.imshow(snapshots['tasks/zeta'][-1,:,:],extent=[0,2*np.pi,0,1],aspect='auto',cmap='viridis',interpolation='none')
 vort_ax.set_xlabel('x')
