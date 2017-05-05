3cef194ae5dc915940c93cbbea51de468b4af132
J.S. Oishi <jsoishi@gmail.com>
2017-05-04 22:11:08 -0400

    fixed two important bugs:
    * taking the derivative for the second cumulant was flawed, caused by overzealous deep copying of bases.
    * properly computing the limits for Fourier and SinCos grids. Chebyshev is still to be done.

--- python/second_cumulant.py
+++ python/second_cumulant.py
@@ -9,6 +9,24 @@ import h5py
 import numpy as np
 import dedalus.public as de
 
+def limits_from_grid(basis_type,grid):
+    if basis_type == 'Fourier':
+        # even grid spacing
+        delta = grid[1] - grid[0]
+        left_edge = grid[0]
+        right_edge = grid[-1] + delta
+    elif basis_type == 'SinCos':
+        delta = grid[1] - grid[0]
+        left_edge = grid[0] - delta/2.
+        right_edge = grid[-1] + delta/2.
+    elif basis_type == 'Chebyshev':
+        # Use Gauss points
+        raise NotImplementedError("Chebyshev limits not yet supported")
+    else:
+        raise ValueError("Unknown basis type:", basis_type)
+    
+    return left_edge, right_edge
+
 def domain_from_file(filename,basis_types,dealias=3/2):
     with h5py.File(filename,'r') as dfile:
         testkey = next(iter(dfile['tasks']))
@@ -20,11 +38,17 @@ def domain_from_file(filename,basis_types,dealias=3/2):
         bases = []
         for n,b,d in zip(dim_names,basis_types,dims):
             if b == 'Fourier':
-                basis = de.Fourier(n, d, dealias=dealias)
+                limits = limits_from_grid('Fourier',dfile['/scales/'+n+'/1.0'])
+                basis = de.Fourier(n, d, interval=limits, dealias=dealias)
             elif b == 'SinCos':
-                basis = de.SinCos(n, d, dealias=dealias)
+                limits = limits_from_grid('SinCos',dfile['/scales/'+n+'/1.0'])
+                basis = de.SinCos(n, d, interval=limits, dealias=dealias)
             elif b == 'Chebyshev':
-                basis = de.Chebyshev(n,d, dealias=dealias)
+                limits = (-1, 1) # limits not implemented for Chebyshev
+                basis = de.Chebyshev(n,d, limits=limits, dealias=dealias)
+            else:
+                raise ValueError("Unknown basis type:", basis_type)
+            
             bases.append(basis)
         d = de.Domain(bases,grid_dtype=np.float64) # hardcoded domain dtype for now
         return d
@@ -55,14 +79,17 @@ def second_cumulant(y, field1,field2=None):
     yarr = field1.domain.get_basis_object('y').grid()
     idx = (np.abs(yarr - y)).argmin()
     
-    x_basis = copy.deepcopy(field1.domain.get_basis_object('x'))
-    y_basis = copy.deepcopy(field1.domain.get_basis_object('y'))
-    xprime_basis = copy.deepcopy(field1.domain.get_basis_object('x'))
-    xprime_basis.name = 'xprime'
+    xb_old = field1.domain.get_basis_object('x')
+    yb_old = field1.domain.get_basis_object('y')
+    x_basis = type(xb_old)(xb_old.name,xb_old.base_grid_size,xb_old.interval,xb_old.dealias)
+    y_basis = type(yb_old)(yb_old.name,yb_old.base_grid_size,yb_old.interval,yb_old.dealias)
+    xprime_basis = type(xb_old)('xprime',xb_old.base_grid_size,xb_old.interval,xb_old.dealias)
     grid_dtype = field1['g'].dtype
     new_domain = de.Domain([y_basis,x_basis,xprime_basis],grid_dtype=grid_dtype)
     sc_raw = new_domain.new_field()
-    sc_raw.meta['y']['parity'] = field1.meta['y']['parity']*field2.meta['y']['parity']
+    if 'parity' in field1.meta['y'].keys():
+        sc_raw.meta['y']['parity'] = field1.meta['y']['parity']*field2.meta['y']['parity']
+    
     sc_raw['g'] = np.einsum('i,jk->jik',field1['g'][idx,:], field2['g'])
     sc = sc_raw.integrate('xprime')
     sc.set_scales('1')
