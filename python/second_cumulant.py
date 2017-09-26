89894a5a2e7716c06e77878e901d7a19f352b79d
J.S. Oishi <jsoishi@gmail.com>
2017-09-26 14:07:47 -0400

    updated version for Steve.

--- python/second_cumulant.py
+++ python/second_cumulant.py
@@ -69,45 +69,83 @@ def fields_from_file(filename,basis_types,field,meta=None,index=-1):
         f[layout] = dset[(index,) + (slice(None),slice(None))]
     return f
 
-def second_cumulant(y, field1,field2=None,layout='yx'):
-    """function to calcluate the second cumulant of data. y is the y
-    component of the r1 vector.
-    """
-    if field2 is None:
-        field2 = field1
+def second_cumulant(y,f,g=None,layout='xy'):
+    """Computes the second cumulant of two fields, f and g
 
+    c_fg = <f'(x1, y1) g'(x2, y2)>
+
+    where <> is an average in the x1 direction. Because of the
+    periodicity in x, x2 = x1 + dx. The y parameter is y2, and is held
+    constant. The cumulant is implemented by computing the
+    x-correlation of f and g at each y1.
+
+    Parameters
+    ----------
+    y : float
+        y2 in the above equation; this is held constant
+    f : dedalus field object
+        the first field
+    g : dedalus field object, optional
+        the second field. if not given, the second cumulant of the f with itself is calculated
+    layout : 'xy' or 'yx'
+        gives the order of the x and y bases in teh domain
+
+    """
+    if g is None:
+        g = f
     if layout == 'yx':
         yx = True
     elif layout == 'xy':
         yx = False
     else:
         raise ValueError("Layout must be one of 'yx' or 'xy'.")
-    
-    yarr = field1.domain.get_basis_object('y').grid()
+    fields = [f,g]
+    for f in fields:
+        xb = f.domain.get_basis_object('x')
+        Lx = xb.interval[1] - xb.interval[0]
+        mean = (f.integrate('x')/Lx).evaluate()
+        mean.set_scales(1.0)
+        f.set_scales(1.0)
+        f['g'] = f['g'] - mean['g']
+    yarr = f.domain.get_basis_object('y').grid()
     idx = (np.abs(yarr - y)).argmin()
-    
-    xb_old = field1.domain.get_basis_object('x')
-    yb_old = field1.domain.get_basis_object('y')
-    x_basis = type(xb_old)(xb_old.name,xb_old.base_grid_size,xb_old.interval,xb_old.dealias)
-    y_basis = type(yb_old)(yb_old.name,yb_old.base_grid_size,yb_old.interval,yb_old.dealias)
-    xprime_basis = type(xb_old)('xprime',xb_old.base_grid_size,xb_old.interval,xb_old.dealias)
-    grid_dtype = field1['g'].dtype
-    if yx:
-        new_domain = de.Domain([y_basis,x_basis,xprime_basis],grid_dtype=grid_dtype)
-    else:
-        new_domain = de.Domain([xprime_basis,x_basis,y_basis],grid_dtype=grid_dtype)
-    sc_raw = new_domain.new_field()
-    if 'parity' in field1.meta['y'].keys():
-        sc_raw.meta['y']['parity'] = field1.meta['y']['parity']*field2.meta['y']['parity']
 
+    outdata = np.empty_like(f['g'])
     if yx:
-        sc_raw['g'] = np.einsum('i,jk->jki',field1['g'][idx,:], field2['g'])
+        for i in range(f['g'].shape[0]):
+            outdata[i,:] = np.correlate(f['g'][i,:],g['g'][idx,:],mode='same')
     else:
-        sc_raw['g'] = np.einsum('i,jk->ijk',field1['g'][:,idx],field2['g'])
-    sc = sc_raw.integrate('xprime')
-    sc.set_scales('1')
+        for i in range(f['g'].shape[1]):
+            outdata[:,i] = np.correlate(f['g'][:,i],g['g'][:,idx],mode='same')
+
+    return outdata
+
+def second_cumulant_tavg(datafile, field1, field2, y, start, stop, layout='xy'):
+    """compute time averaged second cumulant
+
+    inputs
+    ------
+    datafile : hdf5 file containing dedalus outputs
+    field1 : first field for cumulant
+    field2 : second field for cumulant (if the same as field1, will do 2nd cumulant of field with itself)
+    y : y point for comparison
+    start : starting index in datafile
+    start : ending index in datafile
     
-    return sc
+    """
+    cumulants = []
+    for snap in range(start, stop+1):
+        f1 = fields_from_file(datafile,['Fourier','SinCos'],field1,meta={'y':{'scale':1.0,'parity':-1,'constant':False}},index=snap)
+        if field2 != field1:
+            f2 = fields_from_file(datafile,['Fourier','SinCos'],field2,meta={'y':{'scale':1.0,'parity':-1,'constant':False}},index=snap)
+        else:
+            f2 = None
+        c = second_cumulant(y,f1,g=f2,layout='xy')
+        cumulants.append(c)
+    cumulants = np.array(cumulants)
+    
+    return cumulants.mean(axis=0)
+
 
 if __name__ == "__main__":
     basedir = sys.argv[-1]
