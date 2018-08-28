023525d6834dfd05de9a42545a5af0ab697df736
J. S. Oishi <jsoishi@gmail.com>
2018-08-28 18:01:57 +0000

    First rough draft of Diagonal operator
    This is non-functional, but the notebook shows the way forward.

--- python/CE2/diagonal.py
+++ python/CE2/diagonal.py
@@ -85,3 +85,92 @@ class FourierDiagonal(Operator, FutureField):
         # Move back to starting layout
         out.require_layout(arg.layout)
 
+class SinCosDiagonal(Operator, FutureField):
+    """SinCos  interpolation-on-diagonal operator.
+
+    Parameters
+    ----------
+    arg : field object
+        Field argument
+    basis0, basis1 : basis identifiers
+        Bases for diagonal interpolation
+
+    Notes
+    -----
+    The return data is structured such that, for g = Diag(f), and x1,x2 in [a,b],
+        g(x1, x2) = f(x1+x2-a, x1+x2-a)
+    s.t. interpolation of g at x1=a or x2=a yields the diagonalization of f,
+    i.e. f(x, x), arranged as a one-dimensional function of x2 or x1, respectively.
+
+    """
+    def __init__(self, arg, basis0, basis1, **kw):
+        arg = Operand.cast(arg)
+        super().__init__(arg, **kw)
+        self.basis0 = self.domain.get_basis_object(basis0)
+        self.basis1 = self.domain.get_basis_object(basis1)
+        self.axis0 = self.domain.bases.index(self.basis0)
+        self.axis1 = self.domain.bases.index(self.basis1)
+        if self.axis0 > self.axis1:
+            raise ValueError("Cannot evaluate specified axis order.")
+        if self.basis0.interval != self.basis1.interval:
+            raise ValueError("Bases must occupy same interval.")
+        self.name = 'Diag[%s=%s]' %(self.basis0.name, self.basis1.name)
+        # Shear array
+        k0 = reshape_vector(self.basis0.wavenumbers, dim=self.domain.dim, axis=self.axis0)
+        x1 = self.domain.grid(self.axis1, scales=self.domain.dealias)
+        # Filter mask
+        slices = self.domain.dist.coeff_layout.slices(self.domain.dealias)
+        k0 = reshape_vector(self.basis0.wavenumbers[slices[self.axis0]], dim=self.domain.dim, axis=self.axis0)
+        k1 = reshape_vector(self.basis1.wavenumbers[slices[self.axis1]], dim=self.domain.dim, axis=self.axis1)
+        self.filter_mask = (k0 == k1)
+
+    def meta_constant(self, axis):
+        # Preserve constancy
+        return self.args[0].meta[axis]['constant']
+
+    def check_conditions(self):
+        # Shearing layout
+        layout = self.args[0].layout
+        return ((layout.grid_space[self.axis1]) and
+                (not layout.grid_space[self.axis0]) and
+                (layout.local[self.axis0]))
+
+    def operate(self, out):
+        arg = self.args[0]
+        axis0 = self.axis0
+        axis1 = self.axis1
+        # Enforce conditions for shearing space
+        arg.require_grid_space(axis=axis1)
+        arg.require_coeff_space(axis=axis0)
+        arg.require_local(axis=axis0)
+        # Apply Fourier shear to flatten the diagonal
+        # s.t out(y0, y1) = arg(y0+y1-a, y1)
+        out.layout = arg.layout
+        np.multiply(arg.data, self.shear, out=out.data)
+        # Interpolate on flattened diagonal
+        # s.t. out(y0, y1) = arg(y1, y1)
+        self.basis0.Interpolate(out, 'left', out=out).evaluate()
+        out.meta['y0']['constant'] = False
+        # Broadcast and filter coefficients
+        # s.t. out(y0, y1) = arg(y0+y1-a, y0+y1-a)
+        out.data[axslice(axis0, None, None)] = out.data[axslice(axis0, 0, 1)]
+        out.require_coeff_space(axis=axis1)
+        out.data *= self.filter_mask
+        # Move back to starting layout
+        out.require_layout(arg.layout)
+
+        di = []
+        for point in self.basis0.grid():
+            di.append(self.diag_interp(y2,point))
+        di = np.array(di)
+        output.meta[axis0]['parity'] = 1
+        output.meta[axis1]['parity'] = 1
+        output.require_grid_space(axis=axis1)
+        output.require_coeff_space(axis=axis0)
+        output.require_local(axis=axis0)
+        output.data[axslice(axis0,0,1)] = di
+        output.data[axslice(axis0,1,None)] = 0
+        output.data[axslice(axis0,None, None)] = output.data[axslice(axis0,0,1)]
+        output.require_coeff_space(axis=axis1)
+        output.data *= filter_mask
+    
