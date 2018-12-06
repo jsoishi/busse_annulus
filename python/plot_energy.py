dd29f1ef05e19a312bc0813da10a400942377441
J. S. Oishi <jsoishi@gmail.com>
2018-12-06 17:15:10 -0500

    Added DNS energy plotting script
    Will add comparison to CE2 to this...

---
+++ python/plot_energy.py
@@ -0,0 +1,44 @@
+"""
+Plot a spacetime diagram
+
+Usage:
+    plot_spacetime.py [--var=<var>] <datapath>
+
+Options:
+    --var=<var>             variable to plot [default: <x kin en density>_x]
+
+"""
+import os
+import sys
+import matplotlib as mpl
+mpl.use('Agg')
+
+import matplotlib.pyplot as plt
+import h5py
+import numpy as np
+plt.style.use('ggplot')
+mpl.rcParams['font.size'] = 18
+
+import dedalus.extras.plot_tools as dpt
+
+from docopt import docopt
+
+# parse arguments
+args = docopt(__doc__)
+var = args['--var']
+
+datadir = args['<datapath>']
+ts = h5py.File(datadir+"/timeseries/timeseries_s1.h5")
+
+fig = plt.figure(figsize=(14,8))
+ts_ax = fig.add_axes([0.1,0.1,0.85,0.85])
+
+ts_ax.plot(ts['scales/sim_time'][:],ts['tasks/Ekin'][:,0])
+#ts_ax.set_xlim(0.,1.)
+#ts_ax.set_ylim(100,1500)
+ts_ax.set_xlabel('$t$')
+ts_ax.set_ylabel('$E_{kin}$')
+
+savefilename = datadir.rstrip('/')
+savefilename = savefilename.split('/')[1]
+fig.savefig('../figs/{}_kin_energy.png'.format(savefilename))
