177819929434a31437f0d199197eb3358a8476e8
J. S. Oishi <jsoishi@gmail.com>
2019-01-18 09:56:18 -0500

    Added saftey check for new_run.sh

--- python/CE2/new_run.sh
+++ python/CE2/new_run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 name=$1
+if [ -d $name ]; then
+    echo "$1: directory exists."
+    exit
+fi
 mkdir $name
 cd $name
 ln -s ../simulation.py .
@@ -9,3 +13,4 @@ cp ../run_dss_busse.sh .
 ln -s ../plot_profiles.py .
 ln -s ../plot_snapshots.py .
 ln -s ../plot_scalars.py .
+ln -s ../merge.sh .
