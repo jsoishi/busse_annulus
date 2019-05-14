7203eb8f63ac0c9d3e2b1549692cbb5a53fc6b06
J. S. Oishi <jsoishi@gmail.com>
2019-05-14 09:23:37 -0400

    Fixed symmetry transform

--- python/CE2/symmetry.py
+++ python/CE2/symmetry.py
@@ -1,8 +1,18 @@
 import transpose
-def enforce_symmetry(field):
+import reverse
+def enforce_symmetry(field, symm_field=None):
     #logger.info("Enforcing symmetry.")
-    T = transpose.TransposeOperator
-    
-    trans = T(field).evaluate()
+    Trans = transpose.TransposeOperator
+    Rev = reverse.ReverseFirst
 
-    field['g'] = 0.5*(trans['g'] + field['g'])
+    if symm_field:
+        symm = Rev(Trans(symm_field)).evaluate()
+    else:
+        # symmetry transform is transpose y1, y2, then reverse x
+        symm = Rev(Trans(field)).evaluate()
+
+    field['g'] = 0.5*(symm['g'] + field['g'])
+
+    if symm_field:
+        symm = Rev(Trans(field)).evaluate()
+        symm_field['g'] = symm['g']
