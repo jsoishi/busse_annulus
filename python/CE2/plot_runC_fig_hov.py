cc7c82f417478a4542d8070002da7cfa987731b5
J. S. Oishi <jsoishi@gmail.com>
2021-06-08 13:11:12 -0400

    updated run C figure and code

--- python/CE2/plot_runC_fig_hov.py
+++ python/CE2/plot_runC_fig_hov.py
@@ -19,29 +19,48 @@ plt.style.use('prl')
 dns_dir = "../scratch/busse_annulus_ra8.00e+07_beta5.00e+05_C0.00e+00_Pr1.00e+00_filter5.00e-01_nx512_ny256_CFL_xy/"
 ce2_dir = "run_C_DNS_burst_Lx_adjust/"
 ce2_q_dir = "run_C_DNS_quiet/"
+ce2_mi_dir = "run_C/" # maximum ignorance run
 dns = h5py.File(dns_dir+"integrals.h5","r")
 ce2_b = h5py.File(ce2_dir+"data_profiles.h5","r")
 ce2_q = h5py.File(ce2_q_dir+"data_profiles.h5","r")
+ce2_mi = h5py.File(ce2_mi_dir+"data_profiles.h5","r")
 dns_ts_file = h5py.File(dns_dir+"timeseries/timeseries_s1.h5", "r")
 ce2_b_ts_file = h5py.File(ce2_dir+"data_scalars.h5","r")
 ce2_q_ts_file = h5py.File(ce2_q_dir+"data_scalars.h5","r")
+ce2_mi_ts_file = h5py.File(ce2_mi_dir+"data_scalars.h5","r")
 
-aspect_ratio = 26/13.
+aspect_ratio = 26/(13.*1.5)
 fig_W = 10
 fig_H = fig_W/aspect_ratio
 fig = plt.figure(figsize=[fig_W,fig_H])
 
-dns_ce2_cu_q_hov_ax = fig.add_axes([0.1,0.65,0.5,0.25])
-dns_ce2_cu_q_hov_cb_ax = fig.add_axes([0.1,0.9,0.1,0.02])
-#dns_ce2_cu_q_y_ax = fig.add_axes([0.6,0.65,0.07,0.25])
+# in subfig fractions
+subfig_h = 0.5   #height
+subfig_hm = 0.25 #h margin
+subfig_h_cb = 0.04
+subfig_wm = 0.15
+subfig_wm_r = 0.1
+subfig_w_hov = 0.5
+subfig_w_cb = 0.1
+subfig_w_ts = 0.22
 
-dns_ce2_ke_q_ax = fig.add_axes([0.75,0.65,0.22,0.25])
-dns_ce2_ke_b_ax = fig.add_axes([0.75,0.15,0.22,0.25])
+npanels = 3
+subfig_h /= npanels
+subfig_hm /= npanels
+subfig_h_cb /= npanels
+# top to bottom
 
-dns_ce2_cu_b_hov_ax = fig.add_axes([0.1,0.15,0.5,0.25])
-dns_ce2_cu_b_hov_cb_ax = fig.add_axes([0.1,0.4,0.1,0.02])
-#dns_ce2_cu_b_y_ax = fig.add_axes([0.6,0.15,0.07,0.25])
+dns_ce2_cu_q_hov_ax    = fig.add_axes([subfig_wm,(2*npanels-1)*subfig_hm+(npanels-1)*subfig_h,subfig_w_hov,subfig_h])
+dns_ce2_cu_q_hov_cb_ax = fig.add_axes([subfig_wm,(2*npanels-1)*subfig_hm+npanels*subfig_h,subfig_w_cb,subfig_h_cb])
+dns_ce2_ke_q_ax = fig.add_axes([subfig_wm+subfig_w_hov+subfig_wm_r,(2*npanels-1)*subfig_hm+(npanels-1)*subfig_h,subfig_w_ts,subfig_h])
 
+dns_ce2_cu_b_hov_ax = fig.add_axes([subfig_wm,(2*(npanels-1) -1)*subfig_hm+(npanels-2)*subfig_h,subfig_w_hov,subfig_h])
+dns_ce2_cu_b_hov_cb_ax = fig.add_axes([subfig_wm,(2*(npanels-1) -1)*subfig_hm+(npanels-1)*subfig_h,0.1,subfig_h_cb])
+dns_ce2_ke_b_ax = fig.add_axes([subfig_wm+subfig_w_hov+subfig_wm_r,(2*(npanels-1) -1)*subfig_hm+(npanels-2)*subfig_h,subfig_w_ts,subfig_h])
+
+dns_ce2_cu_mi_hov_ax = fig.add_axes([subfig_wm,subfig_hm,subfig_w_hov,subfig_h])
+dns_ce2_cu_mi_hov_cb_ax = fig.add_axes([subfig_wm,subfig_hm+subfig_h,0.1,subfig_h_cb])
+dns_ce2_ke_mi_ax = fig.add_axes([subfig_wm+subfig_w_hov+subfig_wm_r,subfig_hm,subfig_w_ts,subfig_h])
 
 cu_vmin = -1800
 cu_vmax = 1800
@@ -58,9 +77,11 @@ t_total_b = np.concatenate((dns['scales/sim_time'][dns_start_index:trans_index],
 t_total_q = np.concatenate((dns['scales/sim_time'][dns_start_index:quiet_index],ce2_q_time))
 xx_b,yy_b = np.meshgrid(t_total_b, dns['scales/y/1'][:])
 xx_q,yy_q = np.meshgrid(t_total_q, dns['scales/y/1'][:])
+xx_mi,yy_mi = np.meshgrid(ce2_mi['scales/sim_time'][:], ce2_mi['scales/y0/1.0'][:])
 
 cu_q = concatenate_dns_ce2_hov(dns['tasks/<u>_x'][dns_start_index:quiet_index,0,:], ce2_q['tasks/cu'][:,0,0,:])
 cu_b = concatenate_dns_ce2_hov(dns['tasks/<u>_x'][dns_start_index:trans_index,0,:], ce2_b['tasks/cu'][:,0,0,:])
+cu_mi = ce2_mi['tasks/cu'][:,0,0,:]
 
 # burst 
 cu_b_img = dns_ce2_cu_b_hov_ax.pcolormesh(xx_b,yy_b,cu_b.T,shading='nearest', rasterized=True,cmap=cc.cm['bwy'],vmin=cu_vmin, vmax=cu_vmax)#, xlim=(1,t_total_b[-1]))#[0,3,0,1])
@@ -99,6 +120,11 @@ ce2_q_ts_nz     = ce2_q_ts_ekin - ce2_q_ts_ezonal
 dns_ts_ekin = dns_ts_file['tasks/Ekin'][:,0]
 dns_ts_ezonal = dns_ts_file['tasks/E_zonal'][:,0]
 dns_ts_nz = dns_ts_ekin - dns_ts_ezonal
+
+ce2_mi_ts_time = ce2_mi_ts_file['scales/sim_time'][:]
+ce2_mi_ts_ekin   = ce2_mi_ts_file['tasks/KE'][:,0,0,0]/Lx
+ce2_mi_ts_ezonal = ce2_mi_ts_file['tasks/KE_mean'][:,0,0,0]/Lx
+ce2_mi_ts_nz     = ce2_mi_ts_ekin - ce2_mi_ts_ezonal
 
 #ke_total_time = np.concatenate(dns_ts_file['scales/sim_time'][:], time_transition+ce2_ts_file['scales/sim_time'][:])
 #ke_total = np.concatenate(dns_ts_file['scales/sim_time'][:], time_transition+ce2_ts_file['scales/sim_time'][:])
@@ -162,6 +188,28 @@ dns_ce2_ke_q_ax.set_ylim(3e3,1e7)
 dns_ce2_ke_q_ax.set_yticks([1e4,1e5,1e6])
 dns_ce2_ke_q_ax.axvline(quiet_time_transition,color='k',alpha=0.4,linewidth=1.)
 
+
+# maximum ignorance
+cu_mi_img = dns_ce2_cu_mi_hov_ax.pcolormesh(xx_mi,yy_mi,cu_mi.T,shading='nearest', rasterized=True, cmap=cc.cm['bwy'], vmin=cu_vmin, vmax=cu_vmax)#[0,3,0,1])
+cb = fig.colorbar(cu_mi_img, orientation='horizontal',cax=dns_ce2_cu_mi_hov_cb_ax)
+cb.set_label(label=r'$c_{u}$',fontsize=14)
+#dns_ce2_cu_b_hov_ax.set_xticks([1.5,1.6,1.7,1.8])
+dns_ce2_cu_mi_hov_cb_ax.xaxis.tick_top()
+dns_ce2_cu_mi_hov_cb_ax.xaxis.set_label_position('top')
+dns_ce2_cu_mi_hov_cb_ax.tick_params(axis='x', which='major',labelsize=10, pad=2)
+dns_ce2_cu_mi_hov_ax.set_xlabel("t")
+dns_ce2_cu_mi_hov_ax.set_ylabel("y")
+
+
+dns_ce2_ke_mi_ax.plot(ce2_mi_ts_time, ce2_mi_ts_ekin,linewidth=1)
+dns_ce2_ke_mi_ax.plot(ce2_mi_ts_time, ce2_mi_ts_ezonal,linewidth=1,alpha=0.4)
+dns_ce2_ke_mi_ax.plot(ce2_mi_ts_time, ce2_mi_ts_nz,linewidth=1)
+dns_ce2_ke_mi_ax.set_yscale('log')
+dns_ce2_ke_mi_ax.set_xlabel("t")
+dns_ce2_ke_mi_ax.set_ylabel("Kinetic energy",fontsize=14)
+dns_ce2_ke_mi_ax.set_xlim(0.,ce2_mi_ts_time[-1])
+dns_ce2_ke_mi_ax.set_ylim(3e3,1e7)
+dns_ce2_ke_mi_ax.set_yticks([1e4,1e5,1e6])
 
 fig.savefig("../../figs/run_C_fig.png",dpi=300)
 fig.savefig("../../figs/run_C_fig.pdf",dpi=300)
