52f3da08896dc3d79605ce8812484e545401f3d1
J. S. Oishi <jsoishi@gmail.com>
2021-07-07 15:21:17 -0400

    Updated figure 5 to show DNS, burst, max ig.

--- python/CE2/plot_runC_fig_hov.py
+++ python/CE2/plot_runC_fig_hov.py
@@ -62,8 +62,8 @@ dns_ce2_cu_mi_hov_ax = fig.add_axes([subfig_wm,subfig_hm,subfig_w_hov,subfig_h])
 dns_ce2_cu_mi_hov_cb_ax = fig.add_axes([subfig_wm,subfig_hm+subfig_h,0.1,subfig_h_cb])
 dns_ce2_ke_mi_ax = fig.add_axes([subfig_wm+subfig_w_hov+subfig_wm_r,subfig_hm,subfig_w_ts,subfig_h])
 
-cu_vmin = -1800
-cu_vmax = 1800
+cu_vmin = -1200
+cu_vmax = 1200
 dns_start_index = 1500
 trans_index = 1666
 quiet_index = 1690
@@ -73,24 +73,24 @@ ce2_b_time = ce2_b['scales/sim_time'][:]
 if ce2_b['scales/sim_time'][0] == 0:
     ce2_b_time += burst_time_transition
 ce2_q_time = ce2_q['scales/sim_time'][:] + quiet_time_transition
-t_total_b = np.concatenate((dns['scales/sim_time'][dns_start_index:trans_index],ce2_b_time))
-t_total_q = np.concatenate((dns['scales/sim_time'][dns_start_index:quiet_index],ce2_q_time))
-xx_b,yy_b = np.meshgrid(t_total_b, dns['scales/y/1'][:])
+t_total_b = ce2_b_time
+t_total_q = dns['scales/sim_time'][dns_start_index:quiet_index]
+xx_b,yy_b = np.meshgrid(ce2_b_time, ce2_b['scales/y0/1.0'][:])
 xx_q,yy_q = np.meshgrid(t_total_q, dns['scales/y/1'][:])
 xx_mi,yy_mi = np.meshgrid(ce2_mi['scales/sim_time'][:], ce2_mi['scales/y0/1.0'][:])
 
-cu_q = concatenate_dns_ce2_hov(dns['tasks/<u>_x'][dns_start_index:quiet_index,0,:], ce2_q['tasks/cu'][:,0,0,:])
-cu_b = concatenate_dns_ce2_hov(dns['tasks/<u>_x'][dns_start_index:trans_index,0,:], ce2_b['tasks/cu'][:,0,0,:])
+#cu_q = concatenate_dns_ce2_hov(dns['tasks/<u>_x'][dns_start_index:quiet_index,0,:], ce2_q['tasks/cu'][:,0,0,:])
+cu_b = ce2_b['tasks/cu'][:,0,0,:]
 cu_mi = ce2_mi['tasks/cu'][:,0,0,:]
 
 # burst 
-cu_b_img = dns_ce2_cu_b_hov_ax.pcolormesh(xx_b,yy_b,cu_b.T,shading='nearest', rasterized=True,cmap=cc.cm['bwy'],vmin=cu_vmin, vmax=cu_vmax)#, xlim=(1,t_total_b[-1]))#[0,3,0,1])
+cu_b_img = dns_ce2_cu_b_hov_ax.pcolormesh(xx_b,yy_b,cu_b.T,shading='nearest', rasterized=True,cmap=cc.cm['bwy'],vmin=cu_vmin, vmax=cu_vmax)
 cb = fig.colorbar(cu_b_img, orientation='horizontal',cax=dns_ce2_cu_b_hov_cb_ax)
 cb.set_label(label=r'$c_u$',fontsize=14)
 dns_ce2_cu_b_hov_cb_ax.xaxis.tick_top()
 dns_ce2_cu_b_hov_cb_ax.xaxis.set_label_position('top')
 dns_ce2_cu_b_hov_cb_ax.tick_params(axis='x', which='major',labelsize=10, pad=2)
-dns_ce2_cu_b_hov_ax.axvline(burst_time_transition,color='k',alpha=0.4,linewidth=1.)
+#dns_ce2_cu_b_hov_ax.axvline(burst_time_transition,color='k',alpha=0.4,linewidth=1.)
 
 dns_t_avg_start = 1000
 dns_cu_tavg = dns['tasks/<u>_x'][dns_t_avg_start:,0,:].mean(axis=0)
@@ -101,7 +101,7 @@ ce2_cu_q_tavg = ce2_q['tasks/cu'][:,0,0,:].mean(axis=0)
 # dns_ce2_cu_b_y_ax.set_ylim(0,1)
 # dns_ce2_cu_b_y_ax.set_xlabel(r"$c_u$")
 
-dns_ce2_cu_b_hov_ax.set_xticks([1.5,1.6,1.7,1.8])
+dns_ce2_cu_b_hov_ax.set_xticks([burst_time_transition,1.75,1.85])
 dns_ce2_cu_b_hov_ax.set_xlabel("t")
 dns_ce2_cu_b_hov_ax.set_ylabel("y")
 
@@ -130,40 +130,39 @@ ce2_mi_ts_nz     = ce2_mi_ts_ekin - ce2_mi_ts_ezonal
 
 #ke_total_time = np.concatenate(dns_ts_file['scales/sim_time'][:], time_transition+ce2_ts_file['scales/sim_time'][:])
 #ke_total = np.concatenate(dns_ts_file['scales/sim_time'][:], time_transition+ce2_ts_file['scales/sim_time'][:])
-dns_ce2_ke_b_ax.plot(dns_ts_file['scales/sim_time'][:trans_index],dns_ts_ekin[:trans_index],linewidth=1)
-dns_ce2_ke_b_ax.plot(dns_ts_file['scales/sim_time'][:trans_index],dns_ts_ezonal[:trans_index],linewidth=1,alpha=0.4)
-dns_ce2_ke_b_ax.plot(dns_ts_file['scales/sim_time'][:trans_index],dns_ts_nz[:trans_index],linewidth=1)
-dns_ce2_ke_b_ax.set_prop_cycle(None)
+#dns_ce2_ke_b_ax.plot(dns_ts_file['scales/sim_time'][:trans_index],dns_ts_ekin[:trans_index],linewidth=1)
+#dns_ce2_ke_b_ax.plot(dns_ts_file['scales/sim_time'][:trans_index],dns_ts_ezonal[:trans_index],linewidth=1,alpha=0.4)
+#dns_ce2_ke_b_ax.plot(dns_ts_file['scales/sim_time'][:trans_index],dns_ts_nz[:trans_index],linewidth=1)
+#dns_ce2_ke_b_ax.set_prop_cycle(None)
 # dns_ce2_ke_b_ax.plot(ce2_b_ts_time, ce2_b_ts_ekin,linewidth=1,ls=':')
 # dns_ce2_ke_b_ax.plot(ce2_b_ts_time, ce2_b_ts_ezonal,linewidth=1,alpha=0.4,ls=':')
 # dns_ce2_ke_b_ax.plot(ce2_b_ts_time, ce2_b_ts_nz,linewidth=1,ls=':')
-dns_ce2_ke_b_ax.plot(ce2_b_ts_time, ce2_b_ts_ekin,linewidth=1,ls=':')
-dns_ce2_ke_b_ax.plot(ce2_b_ts_time, ce2_b_ts_ezonal,linewidth=1,alpha=0.4,ls=':')
-dns_ce2_ke_b_ax.plot(ce2_b_ts_time, ce2_b_ts_nz,linewidth=1,ls=':')
+dns_ce2_ke_b_ax.plot(ce2_b_ts_time, ce2_b_ts_ekin,linewidth=1)
+dns_ce2_ke_b_ax.plot(ce2_b_ts_time, ce2_b_ts_ezonal,linewidth=1,alpha=0.4)
+dns_ce2_ke_b_ax.plot(ce2_b_ts_time, ce2_b_ts_nz,linewidth=1)
 
 #dns_ce2_ke_b_ax.plot(ce2_q_ts_time, ce2_q_ts_file['tasks/KE'][:,0,0]/(2*np.pi),linewidth=1)
 dns_ce2_ke_b_ax.set_yscale('log')
 dns_ce2_ke_b_ax.set_xlabel("t")
-dns_ce2_ke_b_ax.set_xlim(1.5,ce2_b_ts_time[-1])
+dns_ce2_ke_b_ax.set_xlim(burst_time_transition,ce2_b_ts_time[-1])
 dns_ce2_ke_b_ax.set_ylim(3e3,1e7)
 dns_ce2_ke_b_ax.set_yticks([1e4,1e5,1e6])
-dns_ce2_ke_b_ax.axvline(burst_time_transition,color='k',alpha=0.4,linewidth=1.)
+#dns_ce2_ke_b_ax.axvline(burst_time_transition,color='k',alpha=0.4,linewidth=1.)
 #dns_ce2_ke_b_ax.ticklabel_format(scilimits=[-3, 3])
 dns_ce2_ke_b_ax.set_ylabel("Kinetic energy",fontsize=14)
 
 
-# Quiet restart
+# DNS
 #dns_ce2_cu_b_y_ax.get_yaxis().set_visible(False)
-cu_q_img = dns_ce2_cu_q_hov_ax.pcolormesh(xx_q,yy_q,cu_q.T,shading='nearest', rasterized=True, cmap=cc.cm['bwy'], vmin=cu_vmin, vmax=cu_vmax)#[0,3,0,1])
+cu_q_img = dns_ce2_cu_q_hov_ax.pcolormesh(xx_q,yy_q,dns['tasks/<u>_x'][dns_start_index:quiet_index,0,:].T,shading='nearest', rasterized=True, cmap=cc.cm['bwy'], vmin=cu_vmin, vmax=cu_vmax)#[0,3,0,1])
 cb = fig.colorbar(cu_q_img, orientation='horizontal',cax=dns_ce2_cu_q_hov_cb_ax)
 cb.set_label(label=r'$c_{u}$',fontsize=14)
-dns_ce2_cu_b_hov_ax.set_xticks([1.5,1.6,1.7,1.8])
+#dns_ce2_cu_b_hov_ax.set_xticks([1.5,1.6,1.7,1.8])
 dns_ce2_cu_q_hov_cb_ax.xaxis.tick_top()
 dns_ce2_cu_q_hov_cb_ax.xaxis.set_label_position('top')
 dns_ce2_cu_q_hov_cb_ax.tick_params(axis='x', which='major',labelsize=10, pad=2)
 dns_ce2_cu_q_hov_ax.set_xlabel("t")
 dns_ce2_cu_q_hov_ax.set_ylabel("y")
-dns_ce2_cu_q_hov_ax.axvline(quiet_time_transition,color='k',alpha=0.4,linewidth=1.)
 
 # dns_ce2_cu_q_y_ax.plot(dns_cu_tavg, dns['scales/y/1'][:], label='DNS')
 # dns_ce2_cu_q_y_ax.plot(ce2_cu_q_tavg, ce2_q['scales/y0/1.0'][:], label='CE2')
@@ -177,18 +176,18 @@ dns_ce2_cu_q_hov_ax.axvline(quiet_time_transition,color='k',alpha=0.4,linewidth=
 dns_ce2_ke_q_ax.plot(dns_ts_file['scales/sim_time'][:quiet_index],dns_ts_ekin[:quiet_index],linewidth=1)
 dns_ce2_ke_q_ax.plot(dns_ts_file['scales/sim_time'][:quiet_index],dns_ts_ezonal[:quiet_index],linewidth=1,alpha=0.4)
 dns_ce2_ke_q_ax.plot(dns_ts_file['scales/sim_time'][:quiet_index],dns_ts_nz[:quiet_index],linewidth=1)
-dns_ce2_ke_q_ax.set_prop_cycle(None)
+#dns_ce2_ke_q_ax.set_prop_cycle(None)
 
-dns_ce2_ke_q_ax.plot(ce2_q_ts_time, ce2_q_ts_ekin,linewidth=1,ls=':')
-dns_ce2_ke_q_ax.plot(ce2_q_ts_time, ce2_q_ts_ezonal,linewidth=1,alpha=0.4,ls=':')
-dns_ce2_ke_q_ax.plot(ce2_q_ts_time, ce2_q_ts_nz,linewidth=1,ls=':')
+#dns_ce2_ke_q_ax.plot(ce2_q_ts_time, ce2_q_ts_ekin,linewidth=1,ls=':')
+#dns_ce2_ke_q_ax.plot(ce2_q_ts_time, ce2_q_ts_ezonal,linewidth=1,alpha=0.4,ls=':')
+#dns_ce2_ke_q_ax.plot(ce2_q_ts_time, ce2_q_ts_nz,linewidth=1,ls=':')
 dns_ce2_ke_q_ax.set_yscale('log')
 dns_ce2_ke_q_ax.set_xlabel("t")
 dns_ce2_ke_q_ax.set_ylabel("Kinetic energy",fontsize=14)
-dns_ce2_ke_q_ax.set_xlim(1.5,ce2_q_ts_time[-1])
+dns_ce2_ke_q_ax.set_xlim(1.5,dns_ts_file['scales/sim_time'][quiet_index])
 dns_ce2_ke_q_ax.set_ylim(3e3,1e7)
 dns_ce2_ke_q_ax.set_yticks([1e4,1e5,1e6])
-dns_ce2_ke_q_ax.axvline(quiet_time_transition,color='k',alpha=0.4,linewidth=1.)
+#dns_ce2_ke_q_ax.axvline(quiet_time_transition,color='k',alpha=0.4,linewidth=1.)
 
 
 # maximum ignorance
